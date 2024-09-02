86c8420c23c24b76bb0feac24545dd1fa1de8c0f
ZoitovNurillo <nurillozoitov97@gmail.com>
2024-09-02 22:48:23 +0500

    Add body toggle dark theme

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//toggle dark theme
+const btnToggle = document.getElementById("toggle-btn");
+const btnHeading = document.querySelector(".btn__heading")
+const toggleImg = document.getElementById("toggle__img");
+let text = true;
+
+let toggle = () => {
+  document.body.classList.toggle("dark");
+  if(text) {
+    text = false;
+    btnHeading.textContent = "Light";
+    toggleImg.setAttribute("src", "./images/icon-sun.svg")
+  }else {
+    text = true;
+    btnHeading.textContent = "Dark";
+    toggleImg.setAttribute("src", "./images/icon-moon.svg");
+  }
+}
+
+btnToggle.addEventListener("click", toggle)
