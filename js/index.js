d34e05ae8bd710dc6060f761e8fc81b1cbf90f67
ZoitovNurillo <nurillozoitov97@gmail.com>
2024-09-13 15:24:44 +0500

    Add toggle dark and ligth theme

--- js/index.js
+++ js/index.js
@@ -1,20 +1,19 @@
-//toggle dark theme
-const btnToggle = document.getElementById("toggle-btn");
-const btnHeading = document.querySelector(".btn__heading")
-const toggleImg = document.getElementById("toggle__img");
-let text = true;
+  const btnToggle = document.getElementById("toggle-btn");
+  const btnHeading = document.querySelector(".btn__heading")
+  const toggleImg = document.getElementById("toggle__img");
+  let text = true;
 
-let toggle = () => {
-  document.body.classList.toggle("dark");
-  if(text) {
-    text = false;
-    btnHeading.textContent = "Light";
-    toggleImg.setAttribute("src", "./images/icon-sun.svg")
-  }else {
-    text = true;
-    btnHeading.textContent = "Dark";
-    toggleImg.setAttribute("src", "./images/icon-moon.svg");
+  let toggle = () => {
+    document.body.classList.toggle("dark");
+    if(text) {
+      text = false;
+      btnHeading.textContent = "Light";
+      toggleImg.setAttribute("src", "./images/icon-sun.svg")
+    }else {
+      text = true;
+      btnHeading.textContent = "Dark";
+      toggleImg.setAttribute("src", "./images/icon-moon.svg");
+    }
   }
-}
 
 btnToggle.addEventListener("click", toggle)
